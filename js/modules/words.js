629cd7fc2a4cc4ff914ef9b4ee23b91808ad21e3
Yevgen Sereda <yevgen.sereda@jelvix.com>
2019-02-13 14:40:13 +0200

    l1 done

--- js/modules/words.js
+++ js/modules/words.js
@@ -1,17 +1,30 @@
 function cleanText(str) {
-    let text = str.trim();
+    let text = str.trim().replace(/  +/, " ");
     return text;
 }
 
 function wordsCount(str) {
     let text = cleanText(str);
     let counter = 0;
-    let start = 0;
-    // while(text.indexOf(' ') != -1) {
-    //     start = start + text.indexOf(' ') + 1;
-    //     counter++;
-    // }
-    return text.indexOf(' ');
+    while(text.indexOf(' ') > -1) {
+        counter++;
+        text = text.substr(text.indexOf(' ') + 1);
+    }
+    if (counter > 0) {
+        counter++;
+    }
+    return counter;
 }
 
-export {wordsCount};
+function* getWords(str) {
+    let text = cleanText(str) + " ";
+    let start = 0;
+    let current = text.indexOf(' ', start);
+    while(current !== -1) {
+        yield text.substr(start, current - start);
+        start = current + 1;
+        current = text.indexOf(' ', start);
+    }
+}
+
+export {wordsCount, getWords};
--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
-import {wordsCount} from "./modules/words";
+import {wordsCount, getWords} from "./modules/words";
+import "@babel/polyfill";
 
 window.addEventListener('DOMContentLoaded', function() {
-    let text = "   kk iuou  iui";
+    let text = "   kk iuou  iui ee www ";
     this.console.log(wordsCount(text));
+    for (let word of getWords(text)) {
+        this.console.log(word);
+    }
 });
