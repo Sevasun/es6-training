e39f0fec6c63ba8396ecddc78348af08f9340763
Yevgen Sereda <sevasun@gmail.com>
2019-02-19 00:14:11 +0200

    hw2 ext

--- js/modules/hw.js
+++ js/modules/hw.js
@@ -6,40 +6,20 @@ function watchObj(node, callback){
 class EmailParser{
     constructor(email){
         this.email = email;
+    };
+    get name() {
+        return this.isCorrect ? this.email.substring(0, this.email.indexOf('@')) : null;
+    };
 
-        Object.defineProperty(this, 'name', {
-            get() {
-                if(this.email.indexOf('@') >= 0) {
-                    return this.email.substring(0, this.email.indexOf('@'));
-                } else {
-                    return this.email;
-                }
-            }
-        });
+    get domain() {
+        return this.isCorrect ? this.email.substr(this.email.indexOf('@') + 1) : null;
+    };
 
-        Object.defineProperty(this, 'domain', {
-            get() {
-                if(this.email.indexOf('@') >= 0) {
-                    return this.email.substr(this.email.indexOf('@') + 1);
-                } else {
-                    return this.email;
-                }
-            }
-        });
-
-        Object.defineProperty(this, 'isCorrect', {
-            get() {
-                let validate = 	
-                /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/;
-                console.log(`${validate.test(this.email)} !!!!!`);
-                if(validate.test(this.email.toUpperCase())) {
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-        });
-    };    
+    get isCorrect() {
+        let validate = 	
+        /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/;
+        return validate.test(this.email.toUpperCase());
+    }; 
 };
 
 export {watchObj, EmailParser};
